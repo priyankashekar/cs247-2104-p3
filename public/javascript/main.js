f1a7b9426eb148d51b5c8267c4020862d3ccd440
Sierra Kaplan-Nelson <sierrakaplan-nelson@dn0a229637.sunet>
2014-04-15 11:02:42 -0700

    Video expanding and covered with opacity and recording

--- public/javascript/main.js
+++ public/javascript/main.js
@@ -1,12 +1,25 @@
 // Initial code by Borui Wang, updated by Graham Roth
 // For CS247, Spring 2014
 
+var largeVideoSize = 500;
+var smallVideoSize = 100;
+
+var fb_new_chat_room; 
+var fb_instance_users;
+var fb_instance_stream; 
+var my_color; 
+
+var username;
+
+
 (function() {
 
   var cur_video_blob = null;
   var fb_instance;
 
   $(document).ready(function(){
+    $("#information").hide();
+    $("#second_counter").hide();
     connect_to_chat_firebase();
     connect_webcam();
   });
@@ -25,10 +38,10 @@
     display_msg({m:"Share this url with your friend to join this chat: "+ document.location.origin+"/#"+fb_chat_room_id,c:"red"})
 
     // set up variables to access firebase data structure
-    var fb_new_chat_room = fb_instance.child('chatrooms').child(fb_chat_room_id);
-    var fb_instance_users = fb_new_chat_room.child('users');
-    var fb_instance_stream = fb_new_chat_room.child('stream');
-    var my_color = "#"+((1<<24)*Math.random()|0).toString(16);
+    fb_new_chat_room = fb_instance.child('chatrooms').child(fb_chat_room_id);
+    fb_instance_users = fb_new_chat_room.child('users');
+    fb_instance_stream = fb_new_chat_room.child('stream');
+    my_color = "#"+((1<<24)*Math.random()|0).toString(16);
 
     // listen to events
     fb_instance_users.on("child_added",function(snapshot){
@@ -39,7 +52,7 @@
     });
 
     // block until username is answered
-    var username = window.prompt("Welcome, warrior! please declare your name?");
+    username = window.prompt("Welcome, warrior! please declare your name?");
     if(!username){
       username = "anonymous"+Math.floor(Math.random()*1111);
     }
@@ -49,11 +62,11 @@
     // bind submission box
     $("#submission input").keydown(function( event ) {
       if (event.which == 13) {
-        if(has_emotions($(this).val())){
-          fb_instance_stream.push({m:username+": " +$(this).val(), v:cur_video_blob, c: my_color});
-        }else{
+        // if(has_emotions($(this).val())){
+        //   fb_instance_stream.push({m:username+": " +$(this).val(), v:cur_video_blob, c: my_color});
+        // }else{
           fb_instance_stream.push({m:username+": " +$(this).val(), c: my_color});
-        }
+        //}
         $(this).val("");
       }
     });
@@ -67,7 +80,7 @@
       var video = document.createElement("video");
       video.autoplay = true;
       video.controls = false; // optional
-      video.loop = true;
+      video.loop = false;
       video.width = 120;
 
       var source = document.createElement("source");
@@ -102,12 +115,15 @@
 
     // callback for when we get video stream from user.
     var onMediaSuccess = function(stream) {
+      $("#welcome").hide();
+      $("#information").show();
+      
       // create video element, attach webcam stream to video element
-      var video_width= 160 * 0.7;
-      var video_height= 160 * 0.7;
+      var video_width= smallVideoSize;
+      var video_height= smallVideoSize;
       var webcam_stream = document.getElementById('webcam_stream');
       var video = document.createElement('video');
-      video.className = "videoClass";
+      video.setAttribute("id","myVideo");
       webcam_stream.innerHTML = "";
       // adds these properties to the video
       video = mergeProps(video, {
@@ -119,23 +135,9 @@
       video.play();
       webcam_stream.appendChild(video);
 
-      $("#webcam_stream").click(function() {
-          console.log("Video clicked!");
-          $("#webcam_stream").removeClass("webcam_small");
-          $("#webcam_stream").addClass("webcam_expanded");
-          var video = document.getElementsByClassName("videoClass")[0];
-          video.setAttribute("width", "500");
-          video.setAttribute("height", "500");
-      });
+      enableExpandingVideo(video_width, video_height, stream);
 
-      // $(document.body).keypress(function(event) {
-        
-      //     //var li = pressedKeys[event.keyCode];
-      //     if(event.which== 38 && event.shiftKey){
-      //     event.preventDefault();
-      //     console.log("Expand video!");
-      //   }     
-      // });
+      
 
       // counter
       // var time = 0;
@@ -144,31 +146,7 @@
       //   second_counter.innerHTML = time++;
       // },1000);
 
-      // now record stream in 5 seconds interval
-      var video_container = document.getElementById('video_container');
-      var mediaRecorder = new MediaStreamRecorder(stream);
-      var index = 1;
-
-      mediaRecorder.mimeType = 'video/webm';
-      // mediaRecorder.mimeType = 'image/gif';
-      // make recorded media smaller to save some traffic (80 * 60 pixels, 3*24 frames)
-      mediaRecorder.video_width = video_width/2;
-      mediaRecorder.video_height = video_height/2;
-
-      mediaRecorder.ondataavailable = function (blob) {
-          //console.log("new data available!");
-          video_container.innerHTML = "";
-
-          // convert data into base 64 blocks
-          blob_to_base64(blob,function(b64_data){
-            cur_video_blob = b64_data;
-          });
-      };
-      setInterval( function() {
-        mediaRecorder.stop();
-        mediaRecorder.start(3000);
-      }, 3000 );
-      console.log("connect to media stream!");
+      
     }
 
     // callback if there is an error when we try and get the video stream
@@ -189,6 +167,109 @@
       }
     }
     return false;
+  }
+
+  function enableExpandingVideo(video_width, video_height, stream) {
+
+      $(document).keydown(function(event) {    
+          //Using 13 = enter key
+          //Could use 38 = up or 37 = left
+        if(event.which== 38 && event.shiftKey){
+          event.preventDefault();
+          $("#webcam_stream").click();
+          console.log("Expand video!");
+          //space bar key code = 32
+        } else if($("#webcam_stream").hasClass("webcam_expanded") && event.which==32)  {
+          enableRecording(video_width, video_height, stream);
+        }
+      });
+  
+      $("#webcam_stream").click(function() {
+        console.log("Video clicked!");
+        var video = document.getElementById("myVideo");
+
+        if($("#webcam_stream").hasClass("webcam_small")) {
+          // video.setAttribute("width", "\"" + largeVideoSize + "\"");
+          // video.setAttribute("height", "\"" + largeVideoSize + "\"");
+          video.width = largeVideoSize;
+          video.height = largeVideoSize;
+          $("#webcam_stream").removeClass("webcam_small");
+          $("#webcam_stream").addClass("webcam_expanded");
+          //Change opacity divs
+          $("#topDiv").removeClass("div_small");
+          $("#topDiv").addClass("div_expanded");
+          $("#bottomDiv").removeClass("div_small");
+          $("#bottomDiv").addClass("div_expanded");
+        } else {
+          // video.setAttribute("width", "\"" + smallVideoSize + "\"");
+          // video.setAttribute("height", "\"" + smallVideoSize + "\"");
+          video.width = smallVideoSize;
+          video.height = smallVideoSize;
+          $("#webcam_stream").removeClass("webcam_expanded");
+          $("#webcam_stream").addClass("webcam_small");  
+          //Change opacity divs
+          $("#topDiv").removeClass("div_expanded");
+          $("#topDiv").addClass("div_small");
+          $("#bottomDiv").removeClass("div_expanded"); 
+          $("#bottomDiv").addClass("div_small");
+        }
+      });
+
+  }
+
+  function enableRecording(video_width, video_height, stream) {
+    console.log("connect to media stream!");
+    // now record stream in 5 seconds interval
+    var video_container = document.getElementById('video_container');
+    var mediaRecorder = new MediaStreamRecorder(stream);
+    var index = 1;
+
+    mediaRecorder.mimeType = 'video/webm';
+    // mediaRecorder.mimeType = 'image/gif';
+    // make recorded media smaller to save some traffic (80 * 60 pixels, 3*24 frames)
+    mediaRecorder.video_width = video_width/2;
+    mediaRecorder.video_height = video_height/2;
+
+    mediaRecorder.ondataavailable = function (blob) {
+        //console.log("new data available!");
+        video_container.innerHTML = "";
+
+        // convert data into base 64 blocks
+        blob_to_base64(blob,function(b64_data){
+          cur_video_blob = b64_data;
+          fb_instance_stream.push({m:username+": ", v:cur_video_blob, c: my_color});
+        });
+    };
+
+    //add count down timer in corner of video
+    
+    var second_counter = document.getElementById('second_counter');
+    var count = 3;
+    second_counter.innerHTML = "";
+    $("#second_counter").show();
+    var second_counter_update = setInterval(timer, 1000);
+
+    function timer() {
+      if(count <=0) {
+        //Hide counter after recording and return video to small
+        clearInterval(second_counter_update);
+        $("#second_counter").hide();
+        var video = document.getElementById("myVideo");
+        video.width = smallVideoSize;
+        video.height = smallVideoSize;
+        $("#webcam_stream").removeClass("webcam_expanded");
+        $("#webcam_stream").addClass("webcam_small"); 
+         //Change opacity divs
+        $("#topDiv").removeClass("div_expanded");
+        $("#topDiv").addClass("div_small");
+        $("#bottomDiv").removeClass("div_expanded"); 
+        $("#bottomDiv").addClass("div_small"); 
+        return;
+      }
+      if(count == 3) mediaRecorder.start(3000);
+      second_counter.innerHTML = count--;
+    }
+
   }
 
 
